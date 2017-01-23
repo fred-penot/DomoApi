71d281ee902a08e9b37a14864202f8ab152ae7ba
fred-penot <frederic.penot@gmail.com>
2017-01-23 13:42:35 +0100

    Update Meteo.php

--- src/Controllers/Meteo.php
+++ src/Controllers/Meteo.php
@@ -22,4 +22,40 @@ $meteo->get('/api/prevision/{token}',
     ->before($checkAuth, Application::EARLY_EVENT)
     ->after($jsonReturn);
 
+$meteo->get('/api/get/cities/{token}/{word}',
+    function ($token, $word) use($app) {
+        try {
+            $cities = $app['service.meteo']->getCities($word);
+            if ($cities instanceof \Exception) {
+                throw new \Exception($cities->getMessage());
+            }
+            $app['retour'] = array(
+                "cities" => $cities
+            );
+        } catch (\Exception $ex) {
+            $app['retour'] = $ex;
+        }
+        return new Response();
+    })
+    ->before($checkAuth, Application::EARLY_EVENT)
+    ->after($jsonReturn);
+
+$meteo->get('/api/get/coordinates/city/{token}/{city}/{cp}',
+    function ($token, $city, $cp) use($app) {
+        try {
+            $coordinates = $app['service.meteo']->getCoordinatesCity($city, $cp);
+            if ($coordinates instanceof \Exception) {
+                throw new \Exception($coordinates->getMessage());
+            }
+            $app['retour'] = array(
+                "coordinates" => $coordinates
+            );
+        } catch (\Exception $ex) {
+            $app['retour'] = $ex;
+        }
+        return new Response();
+    })
+    ->before($checkAuth, Application::EARLY_EVENT)
+    ->after($jsonReturn);
+
 return $meteo;
